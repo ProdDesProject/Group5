6344e8639c0133af6a4f4a6e0cf70abab75f01b2
Andreas Scheipers <andreas.scheipers@stud.h-da.de>
2020-11-04 23:40:27 +0200

    Sample coin data.

--- coin_counter/analyzer.py
+++ coin_counter/analyzer.py
@@ -2,5 +2,24 @@ import json
 
 def count_coins(img: bytes):
     result = {
+        'coins': [
+            {
+                'pos': {
+                    'x': 0.321,
+                    'y': 0.7,
+                    'r': 0.1,
+                },
+                'worth': 2.0,
+            },
+            {
+                'pos': {
+                    'x': 0.5,
+                    'y': 0.5,
+                    'r': 0.05,
+                },
+                'worth': 0.05,
+            },
+        ],
+        'worth': 2.05
     }
     return result
