331678861940a896168244c22a28f42a23f0b75e
Marek Fiala <mafialka@gmail.com>
2020-11-13 20:46:03 +0200

    Added convert to JSON

--- find_coins/main.py
+++ find_coins/main.py
@@ -2,6 +2,27 @@
 
 import numpy as np
 import cv2 as cv
+import json
+
+
+def toJSON(arr):
+    data = {}
+    data['coins'] = []
+
+    for i in arr[0, :]:
+        data['coins'].append({
+            "pos": {
+                'x': float(i[0]),
+                'y': float(i[1]),
+                'r': float(i[2]),
+            },
+            'worth': None
+        })
+    data['sum'] = 0.0
+    data['number of coins'] = arr.shape[1]
+
+    with open('output_json.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
 
 
 def imgRead():
@@ -14,9 +35,8 @@ def imgRead():
 
     # convert to gray colour
     cimg = cv.cvtColor(blurImg, cv.COLOR_GRAY2BGR)
-#    cimg = cv.cvtColor(blurImg, cv.COLOR_BGR2GRAY)
 
-    circles = cv.HoughCircles(blurImg, cv.HOUGH_GRADIENT, 1, 20, param1=90, param2=80, minRadius=0, maxRadius=0)
+    circles = cv.HoughCircles(blurImg, cv.HOUGH_GRADIENT, 1, minDist=50, param1=30, param2=80, minRadius=20, maxRadius=120)
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # draw the outer circle
@@ -25,9 +45,12 @@ def imgRead():
         cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
     cv.imwrite('output.png',cimg)
+    return circles
 
 def main():
-    imgRead()
+
+    circles = imgRead()
+    toJSON(circles)
 
 if __name__ == '__main__':
     main()
