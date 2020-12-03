1248855017759b9563bd7ff470975100ec928c81
Marek Fiala <mafialka@gmail.com>
2020-12-03 15:31:34 +0200

    coin-classify and find_coins workes together

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 
+import glob
+from find_coins import main as fc
+from classifier import prediction
+
 def main():
     print('Coin Counter')
 
+    fc.cleanFiles()
+    circles = fc.findCircles()
+    fc.toJSON(circles)
+    pre = prediction.Prediction()
+
+    for im_path in glob.glob('./find_coins/cropped_coins/*.png'):
+        image = pre.convert_image(im_path)
+        predict_result = pre.predict(image)
+        print(predict_result)
 
 if __name__ == '__main__':
     main()
